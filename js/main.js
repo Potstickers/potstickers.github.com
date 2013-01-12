33928294b1b7a6c9e8467772e19c53d4b699e041
Wellington Zhao <tuc16961@temple.edu>
2013-01-12 00:02:52 -0500

    dgksdlgjkls

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 $(document).ready(
 	//handler
 	function(){
-
+		//cache main div that will be frequently modified
+		var content_div = $('#content');
 		//setup nav link highlighting
 		$('nav ul li a').hover(function(){
 			$(this).parent().toggleClass('navselected');
+		});
+		//ajax stuff
+		$('.post a').click(function(){
+			
 		});
 	}
 );
